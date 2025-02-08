cf5a4e51e2bb9ed0e5bd4d52e56b8578b734ef3a
yubinee12 <soongsilyubin@gmail.com>
2025-02-08 14:05:22 -0600

    signupservercomplete

---
+++ code/JavaChip-main/Server/scripts/SignUp.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('signupForm');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); // 기본 제출 동작 중지
+        
+        
+        const formData = {
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value,
+            name: document.getElementById('name').value,
+            nickname: document.getElementById('nickname').value,
+            email: document.getElementById('email').value
+        };
+
+        
+        fetch('/saveacc', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => {
+            if (response.ok) {
+                // 저장 성공 시 Success 페이지로 이동
+                window.location.href = '/Auth/Success/Success.html';
+            }
+        })
+        .catch(error => {
+            console.error('에러 발생:', error);
+        });
+    });
+});
--- code/JavaChip-main/Server/server.js
+++ code/JavaChip-main/Server/server.js
@@ -5,47 +5,25 @@ const url = "mongodb+srv://admin:1234@cluster0.yrinhkn.mongodb.net/?retryWrites=
 
 
 const mongoclient = require('mongodb').MongoClient;
-// const dbName = 'Javachip';
+
 
 const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../pages'))); //정적파일 위함
+app.use('/scripts', express.static(path.join(__dirname, './scripts'))); //js 파일 위함
 
-// async function run() {
-//   try {
-//     const database = client.db('Javachip');
-//     const movies = database.collection('account');
-
-//     const query = { username: 'aa' };
-//     const movie = await movies.findOne(query);
-
-//     console.log(movie);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-
-
-// app.get('/signup', function(req, res){
-//     mydb.collection('account').find().toArray().then(result => {
-//         console.log(result);
-//         res.render('SignUp.html', { data : result });
-//     })  
-
-// });
-
-mongoclient.connect(url)
+//몽고db 연결
+mongoclient.connect(url) 
   .then(client => {
     mydb = client.db('Javachip');
-    mydb.collection('account').find().toArray().then(result => {
-      console.log(result);
-    });
+    //db에 있는 데이터 출력하기
+    // mydb.collection('account').find().toArray().then(result => {
+    //   console.log(result);
+    // });
 
     app.listen(8080, function() {
       console.log("포트 8080으로 서버 대기중...");
@@ -53,15 +31,27 @@ mongoclient.connect(url)
   })
   .catch(err => {
     console.log(err);
-  });
+});
 
+//회원가입
 app.get('/signup', async (req, res) => {
     try {
-        // Since db is already connected, we can use it directly
-        const accounts = await db.collection('account').find().toArray();
-        res.render('SignUp.html', { data: accounts });
+        const accounts = await mydb.collection('account').find().toArray();
+        res.sendFile(path.join(__dirname, '../pages/Auth/SignUp/SignUp.html')); //html 열기
     } catch (error) {
         console.error('Error fetching accounts:', error);
         res.status(500).send('Internal Server Error');
     }
 });
+
+//회원가입 정보 저장
+app.post('/saveacc',function(req,res){
+    mydb.collection('account').insertOne( //db에 정보 저장
+      {username: req.body.username, password : req.body.password, name: req.body.name,
+        nickname: req.body.nickname, email: req.body.email}
+    ).then(result => { 
+      console.log(result);
+      console.log('데이터 추가 성공');
+      res.status(200).send(); // js 에게 성공했다고 알려서 다음 창으로 넘어가게 하기
+    })
+  });
