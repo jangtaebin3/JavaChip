b72a11dea482ab30df0df22aaf72a03d1ae4eba5
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-04-10 15:43:14 +0900

    [STYLE] 클래스 명 수정

--- client/src/layouts/navigationBar/navigationBar.jsx
+++ client/src/layouts/navigationBar/navigationBar.jsx
@@ -1,23 +1,24 @@
 // components/NavigationBar.jsx
 import React from "react";
+import './navigationBar.css';
 
 const navigationBar = () => {
   return (
-    <nav className="navigation-bar">
-      <div className="nav-left">
-        <div className="logo">로고</div>
+    <nav className="navBar">
+      <div className="navLeft">
+        <div className="logo"></div>
       </div>
-      <ul className="nav-menu">
-        <li className="nav-item">캘린더</li>
-        <li className="nav-item">지출 목록</li>
-        <li className="nav-item">지출 분석</li>
-        <li className="nav-item">체크리스트</li>
-        <li className="nav-item active">커뮤니티</li>
+      <ul className="navMenu">
+        <li className="navItem">캘린더</li>
+        <li className="navItem">지출 목록</li>
+        <li className="navItem">지출 분석</li>
+        <li className="navItem">체크리스트</li>
+        <li className="navItem active">커뮤니티</li>
       </ul>
-      <div className="nav-right">
-        <button className="nav-icon user-icon" />
-        <button className="nav-icon alert-icon" />
-        <button className="nav-icon globe-icon" />
+      <div className="navRight">
+        <button className="navIcon userIcon" />
+        <button className="navIcon alertIcon" />
+        <button className="navIcon globeIcon" />
       </div>
     </nav>
   );
