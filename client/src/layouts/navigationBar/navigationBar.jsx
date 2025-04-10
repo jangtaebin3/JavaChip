524e1d6116af63632d5e65a813feb489dfedefd0
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-04-10 16:25:53 +0900

    [FIX] 이미지 구현 방법 조정

--- client/src/layouts/navigationBar/navigationBar.jsx
+++ client/src/layouts/navigationBar/navigationBar.jsx
@@ -1,6 +1,9 @@
 // components/NavigationBar.jsx
 import React from "react";
 import './navigationBar.css';
+import userIcon from '../../assets/images/userIcon.svg';
+import alertIcon from '../../assets/images/alertIcon.svg';
+import globeIcon from '../../assets/images/globeIcon.svg';
 
 const navigationBar = () => {
   return (
@@ -16,9 +19,9 @@ const navigationBar = () => {
         <li className="navItem active">커뮤니티</li>
       </ul>
       <div className="navRight">
-        <button className="navIcon userIcon" />
-        <button className="navIcon alertIcon" />
-        <button className="navIcon globeIcon" />
+        <img src={alertIcon} alt="Alert" className="navIcon alertIcon" />
+        <img src={userIcon} alt="User" className="navIcon userIcon" />
+        <img src={globeIcon} alt="Globe" className="navIcon globeIcon" />
       </div>
     </nav>
   );
