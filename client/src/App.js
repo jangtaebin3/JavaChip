b83c3addab985ce882908a8fa3fe30efed20c2c7
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-05-20 14:33:13 +0900

    [feat] input 컴포넌트 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout.jsx";
+import Auth from "./pages/Auth/Auth.jsx"
 
 
 function App() {
@@ -14,6 +15,10 @@ function App() {
           <Route path="analysis" element={<h1>ExpenseAnalysisPage</h1>} />
           <Route path="checklist" element={<h1>ChecklistPage</h1>} />
           <Route path="community" element={<h1>CommunityPage</h1>} />
+        </Route>
+        <Route path="/auth" element={<h1>Auth</h1>}>
+          <Route path="login" element={<h1>LoginPage</h1>} />
+          <Route path="register" element={<h1>RegisterPage</h1>} />
         </Route>
       </Routes>
     </Router>
