dfa1c394e1c60ce25f5fbbc9e29a4c4b24091ae8
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-05-21 00:51:43 +0900

    [feat] Auth 페이지 배경 적용

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import logo from "../../assets/images/logo.svg"
 import './Auth.css';
 
 const Auth = () => {
   return (
     <div>
-      <Outlet />
+      <div className="authContainer">
+        <div id="Logo">
+          <img src={logo} alt="Logo" />
+          <p id="logoTitle">돈바라기</p>
+        </div>
+        <div className="authContent">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
