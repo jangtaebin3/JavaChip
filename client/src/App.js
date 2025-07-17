e7f24381df8e50729ea7576b6cf1db5574905453
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-07-18 01:03:31 +0900

    [feat] 메인 페이지 레이아웃 분리

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layouts";
+import MainLayout from "./layouts/mainLayout";
 import Auth from "./pages/Auth";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Success from "./pages/Auth/Success";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Main />} />
+        </Route>
         <Route path="/" element={<Layout />}>
           <Route path="calendar" element={<h1>calendar</h1>} />
           <Route path="expenses" element={<h1>ExpenseListPage</h1>} />
