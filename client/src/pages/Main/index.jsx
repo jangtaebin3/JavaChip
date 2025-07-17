b1ac7fe3243d0b469e28c6f38c42edea9f44f156
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-07-18 01:03:10 +0900

    [feat] 임시 메인 페이지 구현

---
+++ client/src/pages/Main/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Main = () => {
+  return (
+    <div className="mainPage">
+      <h1>메인 페이지</h1>
+      <p>여기는 메인 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default Main;
